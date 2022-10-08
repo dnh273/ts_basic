416c432512a5c6f8641234d8a9ae3dcfefab1da8
dnh2703 <dnh2703@gmail.com>
2022-10-08 14:36:02 +0700

    'basic-ts

---
+++ js/index.js
@@ -0,0 +1,9 @@
+let title = 'abc';
+let content = `
+    <p>
+        Xin chafo! ${title}
+    </p>
+`;
+async function getApi() {
+    let result = await fetch('');
+}
---
+++ ts/index.ts
@@ -0,0 +1,11 @@
+let title:string = 'abc';
+
+let content = `
+    <p>
+        Xin chafo! ${title}
+    </p>
+`
+
+async function getApi() {
+    let result = await fetch('');
+}
