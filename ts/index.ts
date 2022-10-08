dfeaff6e4db39994ce62f07ce3feeeabec418005
dnh2703 <dnh2703@gmail.com>
2022-10-08 14:47:22 +0700

    'data_type_tsc'

--- ts/index.ts
+++ ts/index.ts
@@ -1,11 +1,5 @@
-let title:string = 'abc';
-
-let content = `
-    <p>
-        Xin chafo! ${title}
-    </p>
-`
-
-async function getApi() {
-    let result = await fetch('');
-}
+let hoTen: string = "ABC";
+let luongCB: number = 5000;
+let valid: boolean = true;
+let result: undefined = undefined; // Không phần để khai báo thường dùng để nhận giá trị
+let result1: null = null; // Không phần để khai báo thường dùng để nhận giá trị
